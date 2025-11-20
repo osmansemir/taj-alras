16c15ea4d6e97234d6aa1eaa9a01b010fbc664a0
Osman Semir <osmanshek@gmail.com>
2025-11-20 21:41:10 +0400

    Add dark mode

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export function ModeToggle({ className }: { className?: string }) {
+  const { setTheme, theme } = useTheme();
+  const handleThemeChange = () => {
+    const toTheme = theme === "light" ? "dark" : "light";
+    setTheme(toTheme);
+  };
+
+  return (
+    <Button
+      onClick={handleThemeChange}
+      className={cn(
+        " size-5  bg-background text-foreground hover:bg-background hover:text-primary",
+        className,
+      )}
+    >
+      {theme === "light" ? <Moon className="" /> : <Sun className="" />}
+      <span className="sr-only">Theme Toggle</span>
+    </Button>
+  );
+}
