3ac5e03f02337e4b45f80e09d51f547c8250fba9
Osman Semir <osmanshek@gmail.com>
2025-11-27 13:06:07 +0400

    Fix prose in darkmode in contact page

--- app/(allPages)/contact/page.tsx
+++ app/(allPages)/contact/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Contact() {
   return (
     <div className="flex items-center justify-center flex-1">
-      <div className="flex flex-col w-9/10 lg:w-1/2 prose">
+      <div className="flex flex-col w-9/10 lg:w-1/2 prose dark:prose-invert">
         <h1 className="font-sans text-3xl">Contact</h1>
         <p>Please Contact us if you have any questions or concerns.</p>
         <Link href="email@osmansemir.com">info@tajalras.net</Link>
